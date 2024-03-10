32b2e221d534f250d7a81e7b7725f948c8013119
Ganesh Alla <allaganesh03@outlook.com>
2024-03-10 13:57:55 +0530

    Updated prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,16 +1,27 @@
+// import { PrismaClient } from "@prisma/client";
+
+// declare global{
+//     var prisma:PrismaClient
+// }
+
+// if(process.env.NODE_ENV === 'production'){
+//     prisma = new PrismaClient()
+// }
+// else{
+//     if(!global.prisma){
+//         global.prisma = new PrismaClient();
+//     }
+//     prisma = global.prisma;
+// }
+// export default prisma
+
 import { PrismaClient } from "@prisma/client";
 
-declare global{
-    var prisma:PrismaClient
+declare global {
+	var prisma: PrismaClient | undefined;
 }
 
-if(process.env.NODE_ENV === 'production'){
-    prisma = new PrismaClient()
-}
-else{
-    if(!global.prisma){
-        global.prisma = new PrismaClient();
-    }
-    prisma = global.prisma;
-}
-export default prisma
+const prisma = global.prisma || new PrismaClient({ log: ["info"] });
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+
+export default prisma;
