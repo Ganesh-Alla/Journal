89b14dea4c62a36cda358dfde2139aba997654f9
Ganesh Alla <allaganesh03@outlook.com>
2024-03-10 23:15:46 +0530

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,51 +1,10 @@
-"use client"
-import { useState, useEffect, Suspense } from 'react';
-import EntryCard from '@/pages/EntryCard';
-import { Mood } from '@prisma/client';
-import Loading from './loading';
+import Cards from '@/pages/Cards'
+import React from 'react'
 
-export const revalidate = 10;
-
-interface Entry {
-    id: string;
-    title: string;
-    content: string;
-    createdAt: Date;
-    mood: Mood;
+const Home = () => {
+  return (
+    <Cards />
+  )
 }
 
-export default function Home() {
-    const [entries, setEntries] =  useState<Entry[]>([]);
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-        const fetchEntries = async () => {
-            try {
-                const res = await fetch('/api/entry/fetch');
-                if (res.ok) {
-                    const data = await res.json();
-                    setEntries(data.entries);
-                    setLoading(false);
-                } else {
-                    throw new Error('Failed to fetch entries');
-                }
-            } catch (error) {
-                console.error('Error fetching entries:', error);
-            }
-        };
-
-        fetchEntries();
-    }, []);
-
-    if (loading) {
-        return <Loading />;
-    }
-
-    return (
-        <>
-            {entries.map(entry => (
-                <EntryCard key={entry.id} {...entry} />
-            ))}
-        </>
-    );
-}
+export default Home
--- pages/Cards.tsx
+++ pages/Cards.tsx
@@ -3,34 +3,40 @@ import React, { useState, useEffect } from 'react';
 import prisma from '@/lib/prisma';
 import EntryCard from '@/pages/EntryCard';
 import { Mood } from '@prisma/client';
+import Loading from '@/app/loading';
 
 interface Entry {
     id: string;
     title: string;
     content: string;
     createdAt: Date;
-    mood: Mood; // Assuming Mood is defined somewhere
+    mood: Mood;
 }
 
 const Cards = () => {
     const [entries, setEntries] = useState<Entry[]>([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const fetchEntries = async () => {
             try {
-                const entriesData = await prisma.entry.findMany({
-                    orderBy: {
-                        createdAt: 'desc',
-                    },
-                });
-                setEntries(entriesData);
+                const res = await fetch('/api/entry/fetch');
+                if(res){
+                    const data= await res.json();
+                    const newEntries = data.entries
+                    setEntries(newEntries);
+                    setLoading(false);
+                }
             } catch (error) {
                 console.error('Error fetching entries:', error);
             }
         };
-
         fetchEntries();
+        const intervalId = setInterval(fetchEntries, 10000);
+        return () => clearInterval(intervalId);
     }, []);
+
+    if(loading) return <Loading/>
 
     return (
         <>
@@ -42,4 +48,3 @@ const Cards = () => {
 };
 
 export default Cards;
-
