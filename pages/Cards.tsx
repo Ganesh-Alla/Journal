8a5b5933c0d7910c5f8f9ebf41b6e254d6e232c0
Ganesh Alla <allaganesh03@outlook.com>
2024-03-10 23:29:25 +0530

    fail

--- pages/Cards.tsx
+++ pages/Cards.tsx
@@ -1,20 +1,24 @@
+import React, { useState, useEffect } from 'react';
+import prisma from '@/lib/prisma';
+import EntryCard from '@/pages/EntryCard';
 
-  import prisma from '@/lib/prisma';
-  import EntryCard from '@/pages/EntryCard';
+const Cards = async () => {
 
-  const Cards = async () => {
-  "use server";
-  const entries = await prisma.entry.findMany({
-      orderBy: {
-          createdAt: 'desc',
-      },
-  });
-      return (
-          <>
-              {entries.map(entry => (
-                  <EntryCard key={entry.id} {...entry} />
-              ))}
-          </>
-      );
-  };
-  export default Cards;
+"use server"
+const entries = await prisma.entry.findMany({
+    orderBy: {
+        createdAt: 'desc',
+    },
+});
+
+
+    return (
+        <>
+            {entries.map(entry => (
+                <EntryCard key={entry.id} {...entry} />
+            ))}
+        </>
+    );
+};
+
+export default Cards;
